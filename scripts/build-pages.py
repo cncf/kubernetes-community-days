5fff54c0a4ad91173806249c164313ca0ee056d2
lucperkins <lucperkins@gmail.com>
2019-04-05 14:54:06 -0700

    Allow for external links

--- scripts/build-pages.py
+++ scripts/build-pages.py
@@ -5,10 +5,14 @@ yaml = YAML(typ='safe')
 document = open('data/events.yaml', 'r').read()
 events = yaml.load(document)
 
+def link_set(event):
+    return 'link' in event.keys()
+
 for event in events:
-    title = event['title']
-    tag = event['tag']
-    metadata = "---\ntitle: {}\n---\n".format(title)
-    path = "content/events/{}.md".format(tag)
-    with open(path, 'w') as file:
-        file.write(metadata)
+    if not link_set(event):
+        title = event['title']
+        tag = event['tag']
+        metadata = "---\ntitle: {}\n---\n".format(title)
+        path = "content/events/{}.md".format(tag)
+        with open(path, 'w') as file:
+            file.write(metadata)
