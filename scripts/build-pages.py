90e3db1b61d12a2c3075b37de66d76b54ec4dae6
lucperkins <lucperkins@gmail.com>
2019-04-17 09:54:52 -0700

    Fix events script
    Signed-off-by: lucperkins <lucperkins@gmail.com>

--- scripts/build-pages.py
+++ scripts/build-pages.py
@@ -5,14 +5,34 @@ yaml = YAML(typ='safe')
 document = open('data/events.yaml', 'r').read()
 events = yaml.load(document)
 
-def link_set(event):
-    return 'link' in event.keys()
+def event_link_not_set(event):
+    return 'link' not in event.keys()
 
 for event in events:
-    if not link_set(event):
+    if event_link_not_set(event):
+        event_keys = event.keys()
+
         title = event['title']
         tag = event['tag']
-        metadata = "---\ntitle: {}\n---\n".format(title)
+        start = event['start']
+        end = event['end']
+
+        metadata = "---\ntitle: {}\nstart: {}\nend: {}\n".format(title, start, end)
+
+        for key in ['location', 'img', 'foo']:
+            if key in event_keys:
+                metadata += "{}: {}\n".format(key, event[key])
+    
+        if 'social' in event_keys:
+            social_keys = event['social'].keys()
+
+            metadata += "social:\n"
+
+            for key in ['facebook', 'twitter']:
+                if key in social_keys:
+                    metadata += "  {}: {}\n".format(key, event['social'][key])
+
+        metadata += "---\n"
         path = "content/events/{}.md".format(tag)
         with open(path, 'w') as file:
             file.write(metadata)
