5ae721c6b44ce558a6fa79af334bea692157dba3
Luc Perkins <lucperkins@gmail.com>
2019-09-16 13:35:19 +0200

    Add initial implementation (#120)

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,29 @@ function navbarBurgerToggle() {
   });
 }
 
+function navbarLinks() {
+  var comp = location.host;
+
+  $('a').each(function () {
+    var link = undefined === $(this).attr('href') ? '' : $(this).attr('href');
+
+    if (link != '' && link.indexOf(comp) <= -1 && link.length > 0) {
+      if ($(this).text().trim().length == 0 || link.indexOf('javascript(') > -1) {
+        return;
+      }
+  
+      if (link.charAt(0) != '/' && link.charAt(0) != '#') {
+        console.log(link);
+
+        $(this).append('<sup><i class="fas fa-external-link-alt external-link"></i></sup>');
+        $(this).attr('target', '_blank');
+      }
+    }
+  });
+}
+
 
 $(function() {
   navbarBurgerToggle();
+  navbarLinks();
 });
