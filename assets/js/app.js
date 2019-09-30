aac5d21f65e9dbf10b942c771ed8bf5dfab1cd8d
lucperkins <lucperkins@gmail.com>
2019-09-30 12:16:48 -0700

    Add header anchors

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,17 @@
+{{ $home := .IsHome }}
+
+{{ if not $home }}
+function anchorLinks() {
+  const selectors = '.content h1, .content h2, .content h3, .content h4, .content h5, .content h6';
+
+  anchors.options = {
+    icon: '#'
+  }
+
+  anchors.add(selectors);
+}
+{{ end }}
+
 function navbarBurgerToggle() {
   $(".navbar-burger").click(function() {
     $(".navbar-burger").toggleClass("is-active");
@@ -30,4 +44,7 @@ function navbarLinks() {
 $(function() {
   navbarBurgerToggle();
   navbarLinks();
+  {{ if not $home }}
+  anchorLinks();
+  {{ end }}
 });
