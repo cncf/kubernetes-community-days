20e078164c4a9789e6481322899d99e919cb5e93
Luc Perkins <lucperkins@gmail.com>
2020-03-02 12:55:31 -0800

    Convert site JavaScript to Alpine.js (#315)
    * Remove jQuery dependency
    
    Signed-off-by: lucperkins <lucperkins@gmail.com>
    
    * Convert navbar logic to Alpine
    
    Signed-off-by: lucperkins <lucperkins@gmail.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 function anchorLinks() {
-  if ($('.content')) {
+  const content = document.getElementsByClassName(".content");
+
+  if (content) {
     const selectors = '.content h1, .content h2, .content h3, .content h4, .content h5, .content h6';
 
     anchors.options = {
@@ -10,14 +12,6 @@ function anchorLinks() {
   }
 }
 
-function navbarBurgerToggle() {
-  $(".navbar-burger").click(function() {
-    $(".navbar-burger").toggleClass("is-active");
-    $(".navbar-menu").toggleClass("is-active");
-  });
-}
-
-$(function() {
+document.addEventListener('DOMContentLoaded', (e) => {
   anchorLinks();
-  navbarBurgerToggle();
 });
